89969575ed2baa11667ba9286e38fb920d9640c7
sufiyan12q <sufiyankhan12q@gmail.com>
2025-06-19 13:05:36 +0530

    about foreach and map fnc

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # addTwoNumber
 
 i am adding two number using arrow function 
+
+foreach does not return a new array and give undefined output 
+but map funvtion returns a new array and give proper output
---
+++ string.js
@@ -0,0 +1,26 @@
+let string='sufiyan khan'
+
+// for(let str of string){
+//     console.log(str);  
+// }
+
+let array=[1,2,13,4,15,6,7,8]
+
+// for(let arr in array){
+//     console.log(arr);    
+// } 
+// for in use for print indexes of an array or string 
+
+
+// let vb=array.map((val,idx,arr)=>{
+//     // console.log(val);   
+//     return val 
+// })
+// console.log(vb);
+
+
+// let vb2=array.forEach((val,idx,arr)=>{
+//     return val
+    
+// })
+// console.log(vb2);
